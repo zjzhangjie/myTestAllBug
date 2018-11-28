8ca12994dffffe140a19d09dc72c2cce063ed5f4
zooj <018258@cisdi.com.cn>
2018-11-28 10:29:24 +0800

    inputbug

---
+++ src/views/inputBug.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <input ref="input"  v-model="keyWords"   @compositionstart="startInput"  @compositionend="endInput" type="search" placeholder="搜索">
+  </div>
+</template>
+<script>
+    export default {
+        name: "projectTask",
+        data(){
+            return{
+                keyWords:'',
+                cpLock:false,
+            }
+        },
+        watch:{
+            keyWords: function (newval, oldval) {
+                if(newval!==oldval && this.cpLock ==true){
+                    console.log("新输入的值---"+newval);
+                    console.log("旧的值-------"+oldval);
+                }
+            },
+        },
+        methods:{
+            //开始输入的时候
+            startInput(){
+                console.log("开始输入的时候")
+                this.cpLock = false;
+            },
+            //结束输入的时候
+            endInput($event){
+                console.log("结束输入的时候")
+                this.cpLock = true;
+            }
+        }
+    }
+
+</script>
