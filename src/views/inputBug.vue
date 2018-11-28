3dd2e4edfc8e7ad76bff59a5d32d031fea60fef5
zooj <018258@cisdi.com.cn>
2018-11-28 11:03:42 +0800

    加了一个延时器

--- src/views/inputBug.vue
+++ src/views/inputBug.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
-    <input ref="input"  v-model="keyWords"   @compositionstart="startInput"  @compositionend="endInput" type="search" placeholder="搜索">
+    <input ref="input"  v-model="keyWords"   type="search" placeholder="搜索">
+    <!--<input ref="input"  v-model="keyWords"   @compositionstart="startInput"  @compositionend="endInput" type="search" placeholder="搜索">-->
   </div>
 </template>
 <script>
@@ -14,14 +15,17 @@
         },
         watch:{
             keyWords: function (newval, oldval) {
-                if(newval!==oldval && this.cpLock ==true){
-                    console.log("新输入的值---"+newval);
-                    console.log("旧的值-------"+oldval);
+                if(newval!==oldval){
+                    clearTimeout(this.timer);
+                    this.timer = setTimeout(function() {
+                        console.log("新输入的值---"+newval);
+                        console.log("旧的值-------"+oldval);
+                    }, 500);
                 }
             },
         },
         methods:{
-            //开始输入的时候
+            //开始输入的时候,,针对中文，输入英文得时候就失效啦，，，，
             startInput(){
                 console.log("开始输入的时候")
                 this.cpLock = false;
