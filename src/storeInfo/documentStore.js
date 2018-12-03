e991f710e5979daba730ad2a7835451c29919c46
zooj <018258@cisdi.com.cn>
2018-12-03 10:20:33 +0800

    表格拖拽

--- src/storeInfo/documentStore.js
+++ src/storeInfo/documentStore.js
@@ -1,13 +1,47 @@
 
 //状态管理数据:this.$store.state获取，mapState辅助函数
 const states={
+    data:[]//数据
 
 }
 //用来处理数据函数,其接收唯一参数值state，必须为同步函数，以免数据混乱 this.$store.commit(mutationName)，mapMutations辅助函数
 const mutations={
+    //更新项目列表
+    UPDATEPROJECTLIST:function (state,data) {
+        state.data = data;
+    },
 }
 //通过触发mutation实现的数据变化，可以异步操作，this.$store.dispatch(actionName)，mapActions辅助函数
 const actions={
+    getData:function(context, obj) {
+        let p = new Promise(function (resolve,reject) {
+            let template = {
+                'code': '0',
+                'msg': 'success',
+                'data|0-10': [
+                    {
+                        'proj_id': '@id',
+                        'proj_name': '@ctitle',
+                        'creator': '@cname',
+                        'proj_num': '@id',
+                        'operator': '@cname',
+                        'operation_time': /[1-9]{13}/,
+                    }
+                ]
+
+            };
+            let url = pmApi.api.getProjectList;
+            let params = new Object();
+            params.template = template;
+            httpClient.getData(url, params).then(function(data) {
+                context.commit("UPDATEPROJECTLIST",  data.data.data);
+                resolve();
+            },function () {
+                reject();
+            });
+        })
+        return p
+    },
 
 }
 //有些状态需要做二次处理,this.$store.getters.valueName,mapGetters辅助函数
