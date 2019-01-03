06f3bb5de3b312cf03c25709cc45b3ec64f6cb88
zooj <018258@cisdi.com.cn>
2019-01-03 10:41:51 +0800

    表格拖拽

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/inputBug">inputBug</router-link>|
-      <router-link to="/tableDraggle">tableDraggle</router-link>
+      <router-link to="/tableDraggle">tableDraggle</router-link>|
+      <router-link to="/dhtmlGanttx">dhtmlGanttx</router-link>
     </div>
     <router-view/>
   </div>
