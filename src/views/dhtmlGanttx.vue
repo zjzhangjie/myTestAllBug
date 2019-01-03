ffd45aee2d1e56274372a969a79f8889106fc3ae
zooj <018258@cisdi.com.cn>
2019-01-03 09:43:04 +0800

    表格拖拽

---
+++ src/views/dhtmlGanttx.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        甘特图的自己实例化
+    </div>
+</template>
+
+<script>
+    import "../../assets/dhtmlGanttx/codebase/dhtmlxgantt.js";
+    import "../../assets/dhtmlGanttx/codebase/ext/dhtmlxgantt_smart_rendering";
+    import "../../assets/dhtmlGanttx/codebase/locale/locale_cn.js";
+    export default {
+        name: "dhtmlGanttx"
+    }
+</script>
+
+<style scoped>
+
+</style>
