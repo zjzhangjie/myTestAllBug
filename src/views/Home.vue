9e7bdd766ba339d23b877de8e8493ca16b69d8c6
zooj <018258@cisdi.com.cn>
2019-04-11 14:54:57 +0800

    日历你

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <imgViewer></imgViewer>
   </div>
 </template>
 
 <script>
-    import imgViewer from '@zooj/img-viewer';
 // @ is an alias to /src
 
 export default {
   name: 'home',
   components: {
-      imgViewer
-
   }
 }
 </script>
