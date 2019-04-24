84ce56f4e43d67afd6fada4012797c74a9470d1d
zooj <018258@cisdi.com.cn>
2019-04-24 08:37:29 +0800

    日历你

---
+++ src/components/activeTest.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="test">这是一个测试的例子</div>
+</template>
+
+<script>
+    export default {
+        name: "activeTest"
+    }
+</script>
+
+<style scoped>
+    .test{
+        width: 100px;
+        height: 100px;
+        border: 1px solid red;
+
+    }
+
+</style>
